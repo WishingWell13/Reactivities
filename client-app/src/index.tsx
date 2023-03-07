e320f3c4f8369c86e7e78559d9e9b1376ea074a4
WishingWell13 <andy.smithwick@gmail.com>
2023-03-07 14:58:44 -0800

    End of section 3 (fixed an error)

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   // <React.StrictMode> //Some SemanticUI does not work with StrictMode
     <App />
-  {/* </React.StrictMode> */}
+  /* </React.StrictMode> */
 );
 
 // If you want to start measuring performance in your app, pass a function
